2a83c5d54bb879b9d0c0093055c31c172a0975c1
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-03-21 22:26:47 +0100

    Complete Prep Exercise: charAt

--- rithm-interview-prep-practice/charAt.js
+++ rithm-interview-prep-practice/charAt.js
@@ -1,4 +1,7 @@
 // add whatever parameters you deem necessary - good luck!
-function charAt(){
-
+function charAt(str, idx){
+    if (idx > str.length) {
+        return "";
+    }
+    return str[idx];
 }
