f6a1d91af29812d6f2c2ba66dddac1e9156a371a
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-03-22 22:55:01 +0100

    Complete Prep Exercise: stringIncludes

--- rithm-interview-prep-practice/stringIncludes.js
+++ rithm-interview-prep-practice/stringIncludes.js
@@ -1,4 +1,10 @@
 // add whatever parameters you deem necessary - good luck!
-function stringIncludes(){
-
+function stringIncludes(word, char){
+    
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] == char) {
+            return true;
+        }
+    }
+    return false;
 }
