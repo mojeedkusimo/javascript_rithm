7b32d9b9d1e3d39f9af743eb29459448eeebd301
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-03-23 03:56:33 +0100

    Complete Prep Exercise: stringIndexOf

--- rithm-interview-prep-practice/stringIndexOf.js
+++ rithm-interview-prep-practice/stringIndexOf.js
@@ -1,4 +1,10 @@
 // add whatever parameters you deem necessary - good luck!
-function stringIndexOf(){
+function stringIndexOf(word, char){
 
+        for (let i = 0; i < word.length; i++) {
+            if (word[i] == char) {
+                return i;
+            }
+        }
+        return -1;
 }
