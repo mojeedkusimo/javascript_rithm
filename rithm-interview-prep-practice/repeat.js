1fd64166ded7fb710d4fa098c5c3d701cddbcafb
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-03-24 21:18:26 +0100

    Complete Prep Exercise: repeat

--- rithm-interview-prep-practice/repeat.js
+++ rithm-interview-prep-practice/repeat.js
@@ -1,4 +1,11 @@
 // add whatever parameters you deem necessary - good luck!
-function repeat(){
+function repeat(word, count){
 
+    let newWord = "";
+
+    for (let i = 1; i < count + 1; i++) {
+
+        newWord += word;
+    }
+    return newWord;
 }
