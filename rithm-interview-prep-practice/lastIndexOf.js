eae3fcc2c10682dfcdc9398189b9560b925fb8e6
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-03-26 11:00:44 +0100

    Complete Prep Exercise: lastIndexOf

--- rithm-interview-prep-practice/lastIndexOf.js
+++ rithm-interview-prep-practice/lastIndexOf.js
@@ -1,4 +1,10 @@
 // add whatever parameters you deem necessary - good luck!
-function lastIndexOf(){
+function lastIndexOf(array, num){
 
+    for (let i = array.length; i > -1; i--) {
+        if (array[i] == num) {
+            return i;
+        }
+    }
+    return -1;
 }
