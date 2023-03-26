32fa98ae65efffc02b65a04f2022be31790899fe
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-03-26 11:05:08 +0100

    Complete Prep Exercise: max

--- rithm-interview-prep-practice/max.js
+++ rithm-interview-prep-practice/max.js
@@ -1,4 +1,12 @@
 // add whatever parameters you deem necessary - good luck!
-function max(){
+function max(array){
 
+    let maxNum = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > maxNum) {
+            maxNum = array[i];
+        }
+    }
+    return maxNum;
 }
