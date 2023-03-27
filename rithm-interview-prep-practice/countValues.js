f21ba4631c4cfeac60261ee207daf2969ed8fb35
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-03-27 21:56:15 +0100

    Complete Prep Exercise: countValues

--- rithm-interview-prep-practice/countValues.js
+++ rithm-interview-prep-practice/countValues.js
@@ -1,4 +1,13 @@
 // add whatever parameters you deem necessary - good luck!
-function countValues(){
+function countValues(array, num){
 
-}
+    let count = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == num) {
+            count += 1;
+        }
+    }
+
+    return count;
+} 
--- rithm-interview-prep-practice/min.js
+++ rithm-interview-prep-practice/min.js
@@ -1,4 +1,12 @@
 // add whatever parameters you deem necessary - good luck!
-function min(){
+function min(array){
 
+    let minNum = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < minNum) {
+            minNum = array[i];
+        }
+    }
+    return minNum;
 }
