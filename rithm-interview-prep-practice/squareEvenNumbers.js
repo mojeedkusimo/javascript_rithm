c4d1ac8d18458a44648f14f179c0406eff906152
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-03-31 16:35:20 +0100

    Complete Prep Exercise: squareEvenNumbers

--- rithm-interview-prep-practice/squareEvenNumbers.js
+++ rithm-interview-prep-practice/squareEvenNumbers.js
@@ -1,4 +1,12 @@
 // add whatever parameters you deem necessary - good luck!
-function squareEvenNumbers(){
+function squareEvenNumbers(array){
 
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 == 0) {
+            sum += Math.pow(array[i],2);
+        }
+    }
+
+    return sum; 
 }
