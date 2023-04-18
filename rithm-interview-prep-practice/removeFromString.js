1fb5c66419edac2abd883a7522a42dfe1b10cb83
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-04-18 10:43:47 +0100

    Complete Exercise: removeFromString

--- rithm-interview-prep-practice/removeFromString.js
+++ rithm-interview-prep-practice/removeFromString.js
@@ -1,5 +1,19 @@
 // add whatever parameters you deem necessary - good luck!
 function removeFromString(string, start, count){
 
-    console.log(string);
+    let newStr = "";
+
+    for (let i = 0; i < string.length; i++) {
+        if (i >= start && count > 0) {
+            count--;
+            continue
+        } else {
+            
+        newStr+= string[i];
+        }
+    }
+
+    console.log(newStr);
+    return newStr;
+    
 }
