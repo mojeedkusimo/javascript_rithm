91f063a24d50a876296876ec277f82844ebaf596
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-04-19 10:47:15 +0100

    Complete Exercise: includes

--- rithm-interview-prep-practice/includes.js
+++ rithm-interview-prep-practice/includes.js
@@ -1,4 +1,20 @@
 // add whatever parameters you deem necessary - good luck!
-function includes(){
+function includes(collection, value, index){
 
+    if (index == undefined || typeof(collection) == "object" && !Array.isArray(collection)) {
+        for (item in collection) {
+            if (collection[item] == value) {
+                return true;
+            }
+        }
+
+    } else {
+        for (let i = 0; i < collection.length; i++) {
+            if (collection[i] == value && i >= index) {
+                return true;
+            }
+        }
+    }
+
+    return false;
 }
