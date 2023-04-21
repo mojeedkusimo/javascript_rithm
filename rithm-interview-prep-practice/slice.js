bbd021a11bac7effd49ab582a7d17264abce2f8b
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-04-21 20:19:13 +0100

    Complete Exercise: slice

--- rithm-interview-prep-practice/slice.js
+++ rithm-interview-prep-practice/slice.js
@@ -1,4 +1,19 @@
 // add whatever parameters you deem necessary - good luck!
-function slice(){
+function slice(array, start, end){
 
+    let newArray = [];
+
+    if (end == undefined || end > array.length) {
+
+        for (let i = start; i < array.length; i++){
+            newArray.push(array[i]);
+        }
+        
+    } else {
+        for (let i = start; i < end; i++){
+            newArray.push(array[i]);
+        }
+    }
+
+    return newArray;
 }
