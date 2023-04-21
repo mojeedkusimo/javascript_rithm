90e2cd703d32031877b4e247a5e3b8845ffaae1a
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-04-21 20:47:08 +0100

    Complete Exercise: entries

--- rithm-interview-prep-practice/entries.js
+++ rithm-interview-prep-practice/entries.js
@@ -1,4 +1,16 @@
 // add whatever parameters you deem necessary - good luck!
-function entries(){
+function entries(obj){
+
+    let array = [];
+
+    for (keys in obj) {
+        let subArray = [];
+
+        subArray.push(keys);
+        subArray.push(obj[keys]);
+        array.push(subArray);
+    }
+
+    return array;
 
 }
