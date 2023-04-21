fc3cac1d2df7ee9e668261438cba4f6be75f7f5b
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-04-21 20:51:28 +0100

    Complete Exercise: multiples

--- rithm-interview-prep-practice/multiples.js
+++ rithm-interview-prep-practice/multiples.js
@@ -1,4 +1,14 @@
 // add whatever parameters you deem necessary - good luck!
-function multiples(){
+function multiples(num1, num2){
 
+    let array = [];
+
+    for (let i = 1; i < num2 + 1; i++) {
+
+        let x = num1 * i;
+
+        array.push(x);
+    }
+
+    return array;
 }
