7bcf4c709f7c69c828c0a29c335cb4c2fcaf762a
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-04-22 12:02:37 +0100

    Complete Exercise: minMaxKeyInObject

--- rithm-interview-prep-practice/minMaxKeyInObject.js
+++ rithm-interview-prep-practice/minMaxKeyInObject.js
@@ -1,4 +1,45 @@
 // add whatever parameters you deem necessary - good luck!
-function minMaxKeyInObject(){
+function minMaxKeyInObject(obj){
+
+    let array = [];
+
+    for (key in obj) {
+        array.push(Number(key));
+    }
+
+    let maxNum = 0;
+    let diffArray = [];
+    let maxDif = 0;
+    let maxDifIdx = 0;
+    let minNum = 0;
+    let minMaxArr = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > maxNum) {
+            maxNum = array[i];
+        }
+    }
+
+    for (let j = 0; j < array.length; j++) {
+        diffArray.push(maxNum - array[j]);
+    }
+
+    for (let k = 0; k < diffArray.length; k++) {
+        if (diffArray[k] > maxDif) {
+            maxDif = diffArray[k];
+            maxDifIdx = k;
+        }
+    }
+
+    for (let l = 0; l < array.length; l++) {
+        if (l == maxDifIdx) {
+            minNum = array[l];
+        }
+    }
+
+    minMaxArr.push(minNum);
+    minMaxArr.push(maxNum);
+
+    return minMaxArr;
 
 }
