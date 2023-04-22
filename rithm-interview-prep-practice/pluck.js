d0e05467d2602687c48bf3628caf254641b6aaa5
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-04-22 11:04:12 +0100

    Complete Exercise: pluck

--- rithm-interview-prep-practice/pluck.js
+++ rithm-interview-prep-practice/pluck.js
@@ -1,4 +1,11 @@
 // add whatever parameters you deem necessary - good luck!
-function pluck(){
+function pluck(array, key){
 
+    let valueArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        valueArray.push(array[i][key]);
+    }
+
+    return valueArray;
 }
