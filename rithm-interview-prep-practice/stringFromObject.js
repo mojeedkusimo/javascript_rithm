8d731cc3a1483465b2fa00a1bd6645c0db51adbc
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-04-23 14:57:31 +0100

    Complete Exercise: stringFromObject

--- rithm-interview-prep-practice/stringFromObject.js
+++ rithm-interview-prep-practice/stringFromObject.js
@@ -1,4 +1,16 @@
 // add whatever parameters you deem necessary - good luck!
-function stringFromObject(){
+function stringFromObject(obj){
 
+    let str = "";
+
+    for (key in obj) {
+
+        if (str.length == 0) {
+            str += `${key} = ${obj[key]}`;
+        } else {
+            str += `, ${key} = ${obj[key]}`;
+        }
+    }
+
+    return str;
 }
