944050105c807acee44e91e6f120e544f59ae32b
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-04-24 23:47:25 +0100

    Complete Exercise: countNumbers

--- rithm-interview-prep-practice/countNumbers.js
+++ rithm-interview-prep-practice/countNumbers.js
@@ -1,4 +1,15 @@
 // add whatever parameters you deem necessary - good luck!
-function countNumbers(){
+function countNumbers(array){
 
+    let counter = 0;
+    let spaceChecker = /\s+/;
+
+    for (let i = 0; i < array.length; i++) {
+        
+        if (!(isNaN(array[i])) && array[i].length != 0 && !spaceChecker.test(array[i])) {
+            counter++;
+        }
+    }
+
+    return counter;
 }
