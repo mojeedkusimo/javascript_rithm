ecccb483362a6141d3573b2ce0d82b79ed704ae2
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-04-25 09:15:19 +0100

    Complete Exercise: totalCaps

--- rithm-interview-prep-practice/totalCaps.js
+++ rithm-interview-prep-practice/totalCaps.js
@@ -1,4 +1,15 @@
 // add whatever parameters you deem necessary - good luck!
-function totalCaps(){
+function totalCaps(array){
 
+    let capCounter = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array[i].length; j++) {
+            if (array[i][j].charCodeAt() < 91) {
+                capCounter++;
+            }
+        }
+    }
+
+    return capCounter;
 }
