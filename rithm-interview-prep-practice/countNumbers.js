2a5f296e96d1dd68c26d98843779461d272162a7
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-04-25 08:49:47 +0100

    Complete Exercise: removeVowels

--- rithm-interview-prep-practice/countNumbers.js
+++ rithm-interview-prep-practice/countNumbers.js
@@ -1,11 +1,12 @@
 // add whatever parameters you deem necessary - good luck!
-function countNumbers(array){
+'use strict';
+function countNumbers(array) {
 
     let counter = 0;
     let spaceChecker = /\s+/;
 
     for (let i = 0; i < array.length; i++) {
-        
+
         if (!(isNaN(array[i])) && array[i].length != 0 && !spaceChecker.test(array[i])) {
             counter++;
         }
--- rithm-interview-prep-practice/removeVowels.js
+++ rithm-interview-prep-practice/removeVowels.js
@@ -1,4 +1,15 @@
 // add whatever parameters you deem necessary - good luck!
-function removeVowels(){
+function removeVowels(str){
 
+    let vowelsArray = ['a','e','i','o','u'];
+    let newStr = "";
+
+    for (let i = 0; i < str.length; i++) {
+
+        if (!(vowelsArray.includes(str[i].toLowerCase()))) {
+            newStr += str[i];
+        }
+    }
+
+    return newStr;
 }
