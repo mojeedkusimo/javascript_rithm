2266e98dbe97cacf031d2f72449fd1c48b0c8710
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-04-25 09:02:38 +0100

    Complete Exercise: findTheDuplicate

--- rithm-interview-prep-practice/findTheDuplicate.js
+++ rithm-interview-prep-practice/findTheDuplicate.js
@@ -1,4 +1,12 @@
 // add whatever parameters you deem necessary - good luck!
-function findTheDuplicate(){
+function findTheDuplicate(array){
 
+    for (let i = 0; i < array.length; i++) {
+
+        if (array.includes(array[i], i+1)) {
+            return array[i];
+        }
+    }
+
+    return undefined;
 }
