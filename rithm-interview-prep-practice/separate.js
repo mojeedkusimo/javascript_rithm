d760a5b6698338ef88f7e038ed7dc4afeacff778
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-04-26 08:58:36 +0100

    Complete Exercise: separate

--- rithm-interview-prep-practice/separate.js
+++ rithm-interview-prep-practice/separate.js
@@ -1,4 +1,21 @@
 // add whatever parameters you deem necessary - good luck!
-function separate(){
+function separate(array){
 
+    let separateArray = [];
+
+    const populate = (item) => {
+
+        for (let i = 0; i < array.length; i++) {
+
+            if (array[i] == item) {
+                separateArray.push(array[i]);
+            }
+        }
+    }
+
+    populate('cat');
+    populate('water');
+    populate('dog');
+
+    return separateArray;
 }
