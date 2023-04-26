921dcefa9c388aa6904ed1c245ccab29f2b3bd04
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-04-26 09:26:36 +0100

    Complete Exercise: isAlt

--- rithm-interview-prep-practice/isAlt.js
+++ rithm-interview-prep-practice/isAlt.js
@@ -1,4 +1,15 @@
 // add whatever parameters you deem necessary - good luck!
-function isAlt(){
+function isAlt(str){
 
+    const vowelTest = /[aeiou]/ig;
+    const vowelArray = ['a','e','i','o','u'];
+
+    for (let i = 0; i < str.length; i++) {
+
+        if (vowelArray.includes(str[i]) == vowelArray.includes(str[i+1]) && str[i+1] != undefined) {
+            return false;
+        }
+    }
+
+    return true;
 }
