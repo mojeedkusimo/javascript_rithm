660afd634a41d4a327e006339b4ace25f18721a6
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-05-04 22:21:42 +0100

    Partial Complete Exercise: collectOddsAndEvens

--- rithm-interview-prep-practice-part-2/collectOddsAndEvens.js
+++ rithm-interview-prep-practice-part-2/collectOddsAndEvens.js
@@ -1,5 +1,23 @@
-function collectOddsAndEvens() {
+function collectOddsAndEvens(array) {
   // add whatever parameters you deem necessary - good luck!
+
+  let container = {
+    odd: 0,
+    even: 0
+  }
+
+  for (let i = 0; i < array.length; i) {
+
+    if (array[i] % 2 == 0) {
+      container.even++
+    } else {
+      container.odd++
+    }
+    
+  }
+
+  return container;
+
 }
 
 
