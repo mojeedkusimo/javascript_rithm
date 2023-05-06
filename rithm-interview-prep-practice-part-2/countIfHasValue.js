d0da74a8117512560b3786740d16634338ae9199
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-05-06 06:36:03 +0100

    Complete Interview Prep 2: countIfHasValue

--- rithm-interview-prep-practice-part-2/countIfHasValue.js
+++ rithm-interview-prep-practice-part-2/countIfHasValue.js
@@ -1,4 +1,23 @@
-function countIfHasValue() {
+function countIfHasValue(obj, num) {
   // add whatever parameters you deem necessary - good luck!
+
+  let counter = 0;
+
+  const isInArray = (array, searchNum) => {
+
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] == searchNum) {
+        return true;
+      }
+    }
+    return false;
+  }
+  for (key in obj) {
+    if (isInArray(obj[key], num)) {
+      counter++;
+    }
+  }
+
+  return counter;
 }
 
