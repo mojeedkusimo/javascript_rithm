00f0ea3f508e1e517adc4510253efb80cfa9d9a6
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-05-08 22:21:12 +0100

    Partial Complete Interview Prep 2: divideObject

--- rithm-interview-prep-practice-part-2/divideObject.js
+++ rithm-interview-prep-practice-part-2/divideObject.js
@@ -1,3 +1,22 @@
-function divideObject() {
+function divideObject(obj) {
   // add whatever parameters you deem necessary - good luck!
+
+  let numCount = 0;
+  let strCount = 0;
+  let total = [];
+
+  for (key of obj) {
+    if (typeof obj[key] == "string") {
+      for (let i = 0; i < obj[key].length; i++) {
+        strCount++;
+      }
+    } else {
+      numCount++;
+    }
+  }
+  total.push([numCount]);
+  total.push([strCount]);
+
+  // return total;
+  return  obj;
 }
