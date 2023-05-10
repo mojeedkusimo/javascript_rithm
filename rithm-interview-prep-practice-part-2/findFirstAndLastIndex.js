9c2af7491e303999aec476577ec509eca9f43168
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-05-10 22:00:43 +0100

    Complete Interview Prep 2: findFirstMove

--- rithm-interview-prep-practice-part-2/findFirstAndLastIndex.js
+++ rithm-interview-prep-practice-part-2/findFirstAndLastIndex.js
@@ -1,4 +1,23 @@
-function findFirstAndLastIndex() {
+function findFirstAndLastIndex(array, num) {
   // add whatever parameters you deem necessary - good luck!
+
+  let occurenceArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+
+    if (array[i] == num && occurenceArray.length < 3) {
+
+      occurenceArray.push(i);
+    } else {
+      occurenceArray[1] = i;
+    }
+
+  }
+
+  if (occurenceArray.length < 2) {
+    return -1;
+  }
+
+  return occurenceArray;
 }
 
--- rithm-interview-prep-practice-part-2/findFirstMove.js
+++ rithm-interview-prep-practice-part-2/findFirstMove.js
@@ -1,3 +1,21 @@
-function findFirstMove() {
+function findFirstMove(array, char) {
   // add whatever parameters you deem necessary - good luck!
+
+  let coordinate = [];
+
+  for (let i = 0; i < array.length; i++) {
+
+    for (let j = 0; j < array[i].length; j++) {
+
+      if (char == array[i][j]) {
+        coordinate.push(i);
+        coordinate.push(j);
+      }
+    }
+  }
+
+  if (coordinate.length) {
+    return coordinate;
+  }
+  return -1;
 }
