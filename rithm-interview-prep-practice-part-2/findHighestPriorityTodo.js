55eac6577d3291051ab64f054ab0e71101a720c6
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-05-11 21:42:48 +0100

    Complete Interview Prep 2: findHighestPriorityTodo

--- rithm-interview-prep-practice-part-2/findHighestPriorityTodo.js
+++ rithm-interview-prep-practice-part-2/findHighestPriorityTodo.js
@@ -1,3 +1,21 @@
-function findHighestPriorityTodo(todos) {
+function findHighestPriorityTodo(array) {
   // add whatever parameters you deem necessary - good luck!
+
+  let priorityAr = [];
+  let topPriority = 0;
+  let topTask = "";
+
+  for (let i = 0; i < array.length; i++) {
+    for (let key in array[i]) {
+      if (array[i]["priority"] > topPriority) {
+        topPriority = array[i]["priority"];
+        topTask = array[i]["task"];
+      }
+    }
+  }
+
+  priorityAr.push(topTask);
+  priorityAr.push(topPriority);
+
+  return priorityAr;
 }
