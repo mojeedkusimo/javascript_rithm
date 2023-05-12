7db0dc30fd7d7d4af40acf0fb6837c846715fb60
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-05-12 22:09:26 +0100

    Complete Interview Prep 2: replaceAfter

--- rithm-interview-prep-practice-part-2/replaceAfter.js
+++ rithm-interview-prep-practice-part-2/replaceAfter.js
@@ -1,3 +1,12 @@
-function replaceAfter() {
+function replaceAfter(array, num) {
   // add whatever parameters you deem necessary - good luck!
+
+  for (let i = 0; i < array.length; i++) {
+    if (i == num) {
+      array[i] = "Hello";
+      array[i + 1] = "world";
+    }
+  }
+
+  return array;
 }
