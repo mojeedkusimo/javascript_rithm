4d69d9c365914ad38f6cd8a818bc46788739b7f8
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-05-13 21:57:04 +0100

    Complete Interview Prep 2: reverseValues

--- rithm-interview-prep-practice-part-2/reverseValues.js
+++ rithm-interview-prep-practice-part-2/reverseValues.js
@@ -1,5 +1,18 @@
-function reverseValues() {
+function reverseValues(array) {
+    
   // add whatever parameters you deem necessary - good luck!
+
+  let newArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+
+    if (array[i] % 2 == 0) {
+        i+=2;
+      continue;
+    } else {
+      newArray.unshift(array[i]);
+    }
+  }
+
+  return newArray;
 }
-
-
