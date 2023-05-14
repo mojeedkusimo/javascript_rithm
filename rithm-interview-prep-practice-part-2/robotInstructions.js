52cb2c31db750c33c51b33639fa48ad01152813d
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-05-14 14:44:43 +0100

    Complete Interview Prep 2: robotInstructions

--- rithm-interview-prep-practice-part-2/robotInstructions.js
+++ rithm-interview-prep-practice-part-2/robotInstructions.js
@@ -1,4 +1,18 @@
-function robotInstructions() {
+function robotInstructions(array) {
  // add whatever parameters you deem necessary - good luck!
+
+ let direction = {
+    U : 0,
+    D : 0,
+    L : 0,
+    R : 0
+ }
+
+ for (let i = 0; i < array.length; i++) {
+    direction[array[i]]++;
+
+ }
+
+ return direction;
 }
 
