3ea66b261b1247064417ac45a611431e7d42412b
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-05-15 22:39:14 +0100

    Complete Interview Prep 2: skipVowels

--- rithm-interview-prep-practice-part-2/skipVowels.js
+++ rithm-interview-prep-practice-part-2/skipVowels.js
@@ -1,3 +1,17 @@
-function skipVowels() {
+function skipVowels(string) {
   // add whatever parameters you deem necessary - good luck!
+
+  let returnArray = [];
+  let vowels = ["a","e","i","o","u"]
+
+  for (let i = 0; i < string.length; i++) {
+    if (vowels.includes(string[i])) {
+      i++;
+      continue;
+    } else {
+      returnArray.push(string[i]);
+    }
+  }
+
+  return returnArray;
 }
