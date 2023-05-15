55362701c41870793909918227b059abe2cf9683
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-05-15 22:28:17 +0100

    Complete Interview Prep 2: scheduleCheck

--- rithm-interview-prep-practice-part-2/scheduleCheck.js
+++ rithm-interview-prep-practice-part-2/scheduleCheck.js
@@ -1,4 +1,15 @@
-function scheduleCheck() {
+function scheduleCheck(obj1, obj2) {
   // add whatever parameters you deem necessary - good luck!
+
+  let counter = 0;
+
+  for (key in obj1) {
+
+    if ( obj1[key] == obj2[key] ) {
+      counter++;
+    }
+  }
+
+  return counter;
 }
 
